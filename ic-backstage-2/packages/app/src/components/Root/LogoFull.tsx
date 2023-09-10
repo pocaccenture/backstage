bf6351c035c3620a30a91656fcf41312aa602aa0
Ramon Maia <89464900+maiaramon@users.noreply.github.com>
2023-09-10 19:51:58 -0300

    atualização dos arquivos

--- ic-backstage-2/packages/app/src/components/Root/LogoFull.tsx
+++ ic-backstage-2/packages/app/src/components/Root/LogoFull.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 
+import MyLogoFull from './logo/MicrosoftTeams-image.png';
+
 const LogoFull = () => {
   const classes = useStyles(); // Add this line to use the styles
 
   return (
     <img
       className={classes.svg} // Change class to className
-      src="https://s3-nao-apagar-de-jeito-nenhum.s3.amazonaws.com/TEF_BIG.D-febe3646.png"
+      src={MyLogoFull}
       alt="LogoFull"
     />
   );
@@ -19,11 +21,6 @@ const useStyles = makeStyles({
     height: 'auto', // Set to auto for responsive resizing
     maxHeight: 300, // Set the maximum height to 300 pixels
   },
-  path: {
-    fill: 'pink',
-    stroke: 'pink'
-  },
 });
 
 export default LogoFull;
-
